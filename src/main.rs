cbed792663d66ef836aa1ed7eb3f7838520b727c
Tyler Zars <tyler@zars.me>
2024-04-08 20:58:52 -0400

    Add learning comments to code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 //use std::io::Result;
-use serde_json::{Value};
 
 #[tokio::main]
 async fn main() {   
@@ -7,18 +6,23 @@ async fn main() {
 
     println!("Grabbing from API");
     let api_body: String = get_court_json().await;
+    //api_body.push('b'); // This will break the JSON parsing and cause parse_json_data() to throw back an error message
     //println!("{}", api_body);
 
     let parsed = parse_json_data(&api_body);
     
     match parsed {
+        // If is_ok()
         Ok(value) => {
+            // Interpt value in ok() as Object, which Some converts to a HashMap
             if let Some(obj) = value.as_object() {
+                // Loop through all Key strings in HashMap
                 for key in obj.keys() {
                     println!("{}", key);
                 }
             }
         }
+        // If is_err()
         Err(e) => println!("Error parsing JSON: {}", e),
     }
 }
@@ -37,7 +41,9 @@ async fn get_court_json() -> String {
     body
 }
 
-fn parse_json_data(data: &String) -> Result<Value, serde_json::Error> {
-    let v: Value = serde_json::from_str(&data)?;
+fn parse_json_data(data: &String) -> Result<serde_json::Value, serde_json::Error> {
+    // This will return a serde_json::Error if it fails
+    let v: serde_json::Value = serde_json::from_str(&data)?;
+    // This will return an serde_json::Value if previous line succeeds
     Ok(v)
 }
